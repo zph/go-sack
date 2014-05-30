3734499ff95889248a9fbc1d9b72c9630f5dee06
Zander Hill <Zander@civet.ws>
2014-05-30 18:24:31 -0400

    Version bump

--- sack.go
+++ sack.go
@@ -147,7 +147,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Sack"
 	app.Usage = "sack [searchterm] [optional directory]"
-	app.Version = "0.2.0"
+	app.Version = "0.2.2"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{"edit, e", "edit a given shortcut"},
 		cli.BoolFlag{"search, s", "search-ack/ag it"},
