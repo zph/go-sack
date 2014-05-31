1cfe39b53e98ba08deccf241a8581b7a1ee35b7f
Zander Hill <Zander@civet.ws>
2014-05-31 02:49:46 -0400

    Add todo to shell_init.go

--- sack/shell_init.go
+++ sack/shell_init.go
@@ -19,5 +19,20 @@ func shellInit(c *cli.Context) {
 func shellEval(c *cli.Context) {
 	sh := "eval \"$(sack init)\""
 	fmt.Println(sh)
-
 }
+
+/*
+// TODO: Add bash and zsh autocomplete
+
+    _cli_bash_autocomplete() {
+        local cur prev opts base
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        opts=$( ${COMP_WORDS[@]:0:COMP_CWORD} --generate-bash-completion )
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    }
+
+    complete -F _cli_bash_autocomplete $PROG
+*/
